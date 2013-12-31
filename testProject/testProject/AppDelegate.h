a6ede43ef1d0626c18200564e0887018ed7b93f0
akshay <akshay1188@gmail.com>
2013-12-31 15:50:42 +0530

    initial commit

---
+++ testProject/testProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by Akshay Mhatre on 31/12/13.
+//  Copyright (c) 2013 raweng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testProject/testProjectTests/testProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  testProjectTests.m
+//  testProjectTests
+//
+//  Created by Akshay Mhatre on 31/12/13.
+//  Copyright (c) 2013 raweng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testProjectTests : XCTestCase
+
+@end
+
+@implementation testProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
